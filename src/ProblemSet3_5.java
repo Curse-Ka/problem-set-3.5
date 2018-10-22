a045194f49194d95624f1106ce10050714736593
William Krska <curse.ka@gmail.com>
2018-10-22 10:17:51 -0400

    first draft of pset

--- src/ProblemSet3_5.java
+++ src/ProblemSet3_5.java
@@ -22,6 +22,10 @@ public class ProblemSet3_5 {
 		// test your solutions here
 		
 		ps.primes(1, 1000);
+		ps.leapYears(5);
+		ps.palindromicNumbers(123454321);
+		ps.fibonacci(12);
+		ps.multiples(5, 6, 120);
 	}
 	
 	/**
@@ -36,7 +40,37 @@ public class ProblemSet3_5 {
 	 */
 	
 	public void primes(int start, int end) {
+		int primeCount = 0;
+		boolean prime = false;
 		
+		for (int cnt = 1; cnt <= end; cnt++) {
+			prime = true;
+			
+			if (cnt == 0 || cnt == 1) {
+				prime = false;
+			}
+			
+			double sqrt = Math.sqrt(cnt);
+			for (int cnt2 = 2; cnt2 < (sqrt + 1); cnt2++) {
+				if (cnt % cnt2 == 0) {
+					prime = false;
+				}
+			}
+			
+			if (cnt == 2) {
+				prime = true;
+			}
+			
+			if (prime) {
+				primeCount++;
+			}
+			
+		}
+		if (primeCount == 0 || primeCount > 1) {
+			System.out.println("There are " + primeCount + " prime numbers");
+		} else {
+			System.out.println("There is " + primeCount + " prime number");
+		}
 	}
 	
 	/**
@@ -50,7 +84,22 @@ public class ProblemSet3_5 {
 	 */
 	
 	public void leapYears(int count) {
+		if (count == 1) {
+			System.out.println("The next leap year is 2020.");
+			return;
+		} else {
+			System.out.print("The next " + count + " leap years are 2020");
+		}
 		
+		int year = 2024;
+		for (int cnt = 1; cnt < (count - 1); cnt++) {
+			System.out.print(", " + year);
+			year += 4;
+		}
+		if (count > 2) {
+			System.out.print(",");
+		}
+		System.out.println(" and " + year + ".");
 	}
 	
 	/**
@@ -63,7 +112,27 @@ public class ProblemSet3_5 {
 	 */
 	
 	public void palindromicNumbers(int number) {
+		int size = 0;
+		int numCopy = number;
+		do {
+			size = (numCopy > 0) ? (size + 1) : (size);
+			numCopy /= 10;
+		} while (numCopy > 0);
+				
+		int lowDig;
+		int highDig;
+		boolean palendromic = true;
 		
+		for (int cnt = 1; cnt <= size/2; cnt++) {
+			lowDig = (number % (int) (Math.pow(10, cnt))) / (int) (Math.pow(10,(cnt - 1)));
+			highDig = (number / (int) (Math.pow(10,(size - (cnt))))) % 10;
+			palendromic = (lowDig == highDig) ? true : false;
+		}
+		if (palendromic) {
+			System.out.println(number + " is a palendromic number");
+		} else {
+			System.out.println(number + " is not a palendromic number");
+		}
 	}
 	
 	/**
@@ -78,7 +147,36 @@ public class ProblemSet3_5 {
 	 */
 	
 	public void fibonacci(int n) {
+		int num1 = 1;
+		int num2 = 1;
+		int sum = 1;
+		for (int cnt = 0; cnt < (n - 2); cnt++) {
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+		}
 		
+		String appendage;
+		switch (n % 10) {
+			case 1:
+				appendage = "st";
+				break;
+			case 2:
+				appendage = "nd";
+				break;
+			case 3:
+				appendage = "rd";
+				break;
+			default:
+				appendage = "th";
+		
+		}
+		
+		if (n > 10 && n < 20) {
+			appendage = "th";
+		}
+		
+		System.out.println("The " + n + appendage + " Fibbonacci number is " + sum);
 	}
 	
 	/**
@@ -91,6 +189,13 @@ public class ProblemSet3_5 {
 	 */
 	
 	public void multiples(int x, int y, int limit) {
-		
+		long sum = 0;
+		for (int count = 1; count <= limit; count++) {
+			if (count % x == 0 && (count / x) % y == 0) {
+				sum += count;
+			}
+		}
+		System.out.println(sum);
 	}
+	
 }
